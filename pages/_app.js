3b346fb01f0ac3058dc84e86a5f2400e599bb309
twenz235 <t.wen.z.235@gmail.com>
2021-12-29 21:57:57 +0700

    init Antd

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '../styles/styles.less';
+import '../styles/responsive.less';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
